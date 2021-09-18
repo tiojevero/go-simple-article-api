5458c9d5ba571c5b1c7ca1bf8b466fe774286700
tiojevero <tiojevero@gmail.com>
2021-09-18 12:39:17 +0700

    Remove Unnecessary comments

--- db.go
+++ db.go
@@ -37,31 +37,3 @@ func Connect() error {
 	return nil
 }
 
-
-
-// func GetMongoDBConnection() (*mongo.Client, error) {
-// 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	err = client.Ping(context.Background(), readpref.Primary())
-	
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	return client, nil
-// } 
-
-// func getMongoDBCollection(DBName string, CollectionName string) (*mongo.Collection, error) {
-// 	client, err := GetMongoDBConnection()
-
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	collection := client.Database(DBName).Collection(CollectionName)
-
-// 	return collection, nil
-// }
