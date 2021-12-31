903a352aef163fb5288da5b3aebeea9869d01bf6
Tio Jevero <39867643+tiojevero@users.noreply.github.com>
2021-12-31 23:02:16 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,9 +14,6 @@ const mongoURI = "mongodb://localhost:27017"
 func Router(app *fiber.App) {
 	app.Get("/article/", GetArticle)
 	app.Put("/article/:id", UpdateArticle)
-	app.Get("/halo", func(c *fiber.Ctx) error {
-		return c.SendString("Hallo Bro")
-	})
 	app.Post("/article", CreateArticle)
 	app.Delete("/article/:id", DeleteArticle)
 }
